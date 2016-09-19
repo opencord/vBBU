faab9e27521d0c2cdf34c36cd99b6327760d56eb
Pingping Lin <pingping@onlab.us>
2016-09-19 23:44:54 +0000

    delete VPGWCComponent Because the location change
    Change-Id: Ic1dd554e5ad3c076f89402128e2392b249c49557

--- xos/admin.py
+++ xos/admin.py
@@ -4,7 +4,7 @@ from core.middleware import get_request
 from core.models import User
 from django import forms
 from django.contrib import admin
-from services.vbbu.models import MCORDService, VBBUComponent, VPGWCComponent, MCORD_KIND
+from services.vbbu.models import MCORDService, VBBUComponent, MCORD_KIND
 
 # The class to provide an admin interface on the web for the service.
 # We do only configuration here and don't change any logic because the logic
